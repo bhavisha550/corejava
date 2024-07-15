a4ea0a3dd41054b51e674e5146ae1a557464c3e7
bhavisha550 <bhavishamoghe@gmail.com>
2024-07-15 15:19:57 +0530

    super

---
+++ com/company/Super.java
@@ -0,0 +1,19 @@
+class Person {
+    public void work(){
+        System.out.println("person is working");
+    }
+}
+
+class Patient extends Person {
+    public void work(){
+        super.work();
+        System.out.println("patient is working");
+    }
+}
+
+public class Super {
+    public static void main(String[] args) {
+        Person p = new Patient();
+        p.work();
+    }
+}
